4f47396816f5191073e47699a1f8095b850307ad
Jimmy Ko <ko.jimmy09@gmail.com>
2023-12-20 14:39:47 -0500

    configure IngredientAdmin for Ingredient model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Ingredient
 
-# Register your models here.
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_filter = ("name", "created_on", "last_modified")
+    list_display = ("name", "created_on", "last_modified")
+
+
+admin.site.register(Ingredient, IngredientAdmin)
