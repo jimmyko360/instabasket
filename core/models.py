14eca8ba8c5681ada27952a3922fbc81107db60b
Jimmy Ko <ko.jimmy09@gmail.com>
2023-12-21 17:07:31 -0500

    enforce unique ingredient names

--- core/models.py
+++ core/models.py
@@ -8,7 +8,7 @@ metric_weight_choices = ["mg", "g", "kg"]
 
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
