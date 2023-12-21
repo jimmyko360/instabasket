105bcf19253571f3cc0f5b2bab5dc57f8a9e4273
Jimmy Ko <ko.jimmy09@gmail.com>
2023-12-20 23:12:15 -0500

    configure admin for Quantity model

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Ingredient
+from .models import Ingredient, Quantity
 
 
 class IngredientAdmin(admin.ModelAdmin):
@@ -7,4 +7,28 @@ class IngredientAdmin(admin.ModelAdmin):
     list_display = ("name", "created_on", "last_modified")
 
 
+class QuantityAdmin(admin.ModelAdmin):
+    list_filter = (
+        "ingredient",
+        "quantity",
+        "unit",
+        "system",
+        "attribute",
+        "conversion_multiple",
+        "created_on",
+        "last_modified",
+    )
+    list_display = (
+        "ingredient",
+        "quantity",
+        "unit",
+        "system",
+        "attribute",
+        "conversion_multiple",
+        "created_on",
+        "last_modified",
+    )
+
+
 admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Quantity, QuantityAdmin)
