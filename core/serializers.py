25941aae7111405c6308a4df8a5408325eaf2bce
Jimmy Ko <ko.jimmy09@gmail.com>
2023-12-30 02:11:03 -0500

    add ListSerializer and RecipeSerializer

--- core/serializers.py
+++ core/serializers.py
@@ -35,6 +35,8 @@ class IngredientSerializer(serializers.ModelSerializer):
 
 
 class QuantitySerializer(serializers.ModelSerializer):
+    # owner = serializers.ReadOnlyField(source="owner.username")
+
     # reverse relationship with Recipe model
     recipes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
@@ -74,6 +76,70 @@ class QuantitySerializer(serializers.ModelSerializer):
             "last_modified",
             "recipes",
             "lists",
+        ]
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    # owner = serializers.ReadOnlyField(source="owner.username")
+
+    # reverse relationship with List model
+    lists = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    created_on = serializers.DateTimeField(read_only=True)
+    last_modified = serializers.DateTimeField(read_only=True)
+
+    def to_representation(self, instance):
+        user_timezone = self.get_user_timezone()
+        created_on = instance.created_on.astimezone(user_timezone)
+        last_modified = instance.last_modified.astimezone(user_timezone)
+        representation = super().to_representation(instance)
+        representation["created_on"] = created_on.strftime("%x %X %Z")
+        representation["last_modified"] = last_modified.strftime("%x %X %Z")
+
+        return representation
+
+    def get_user_timezone(self):
+        user_timezone = (
+            self.context["request"].user.tzinfo if "request" in self.context else None
+        )
+        return user_timezone
+
+    class Meta:
+        model = Recipe
+        fields = ["id", "name", "ingredients", "created_on", "last_modified", "lists"]
+
+
+class ListSerializer(serializers.ModelSerializer):
+    # owner = serializers.ReadOnlyField(source="owner.username")
+
+    created_on = serializers.DateTimeField(read_only=True)
+    last_modified = serializers.DateTimeField(read_only=True)
+
+    def to_representation(self, instance):
+        user_timezone = self.get_user_timezone()
+        created_on = instance.created_on.astimezone(user_timezone)
+        last_modified = instance.last_modified.astimezone(user_timezone)
+        representation = super().to_representation(instance)
+        representation["created_on"] = created_on.strftime("%x %X %Z")
+        representation["last_modified"] = last_modified.strftime("%x %X %Z")
+
+        return representation
+
+    def get_user_timezone(self):
+        user_timezone = (
+            self.context["request"].user.tzinfo if "request" in self.context else None
+        )
+        return user_timezone
+
+    class Meta:
+        model = List
+        fields = [
+            "id",
+            "title",
+            "recipes",
+            "ingredients",
+            "created_on",
+            "last_modified",
         ]
 
 
