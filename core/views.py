ff98d6cd5a07bd60dc5e39ec78276291b125a9b9
Jimmy Ko <ko.jimmy09@gmail.com>
2023-12-31 02:50:06 -0500

    create IngredientViewSet in views.py

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from core.models import Ingredient, Quantity, Recipe, List
+from core.serializers import (
+    IngredientSerializer,
+    QuantitySerializer,
+    RecipeSerializer,
+    ListSerializer,
+)
+
+
+class IngredientViewSet(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    # def perform_create(self, serializer):
+    #     serializer.save(owner=self.request.user)
